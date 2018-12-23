0f9c38a82e39f57b41091e56a83bd4c88ef7a5c0
Noushin Nabavi <nabavinoushin@gmail.com>
2018-12-23 00:48:30 -0800

    JAVA

--- Data Analysis Lessons/4. JAVA WITH R/JAVA_with_R.R
+++ Data Analysis Lessons/4. JAVA WITH R/JAVA_with_R.R
@@ -1,181 +1,134 @@
 ### DEMO for Bringing JavaScript to RStudio for Visualization
-# Lessons are adapted, created, and organized by Noushin Nabavi, PhD.
+#Demo of 4 htmlwidgets: A. Time-Series Plot, B. Heatmap, C. 3D Scatterplot, D. 3D Globe  
+#Lessons are adapted, created, and organized by Noushin Nabavi, PhD.
 
 
-### What are htmlwidgets for R? http://www.htmlwidgets.org  
-###  A growing number of htmlwidget R libraries have been developed from popular JavaScript charting libraries, such as D3.js, plotly.js, Leaflet.js etc.  
+#What are htmlwidgets for R? http://www.htmlwidgets.org  
+#A growing number of htmlwidget R libraries have been developed from popular JavaScript charting libraries, such as D3.js, plotly.js, Leaflet.js etc.  
+#This htmlwidgets framework/interface therefore allows R users to generate interactive JavaScript data visualisations using R code, through the normal operation of loading relevant libraries and calling appropriate functions.
+#These interactive plots can be displayed in RStudio, embedded in R markdown documents and Shiny web apps, and shared in standalone html files over email, dropbox etc.
 
-### This htmlwidgets framework/interface therefore allows R users to generate interactive JavaScript data visualisations using R code, through the normal operation of loading relevant libraries and calling appropriate functions.
+#What Use-Cases do we have for htmlwidgets for R? 
+#1. Data Storytelling: presenting & communicating findings   
+#2. Exploratory Data Analysis (EDA) 
 
-### These interactive plots can be displayed in RStudio, embedded in R markdown documents and Shiny web apps, and shared in standalone html files over email, dropbox etc.
-
-
-#### Demo of 4 htmlwidgets: A. Time-Series Plot, B. Heatmap, C. 3D Scatterplot, D. 3D Globe  
-
-
-### What Use-Cases do we have for htmlwidgets for R? 
-### 1. Data Storytelling: presenting & communicating findings   
-### 2. Exploratory Data Analysis (EDA) 
-
-
-
-#### Install Packages for all the following exercises
-
-
-install.packages("dygraphs")
-install.packages("xts")
-install.packages("d3heatmaps")
-install.packages("threejs")
-
+______________________________________________________________________________________
 
 ### A. Time-Series Plot
-Produce an interactive time-series visualisation using the `dygraphs` R library. Objects for plotting must be in `xts` format (extensible time series).
-
+Produce an interactive time-series visualisation using the `dygraphs` R library. 
+Objects for plotting must be in `xts` format (extensible time series).
 Time-series data = Australian wine sales in no. of bottles, by wine makers, between Jan 1980 - Aug 1994.   
-
 Using the `stl` function to perform the seasonal trend decomposition, which extracts the seasonal component, trend, component and remainder from the original data.
 
 #### (i) Call dygraph & xts libraries
 
-```{r install and call dygraph, message=FALSE}
 library(dygraphs)
 library(xts)
-```
+
 
 #### (ii) Load Data
-In this instance the Australian Wine dataset `wineind` being used is from the `forecast` package so we load this first, then call the data.    
+### In this instance the Australian Wine dataset `wineind` being used is from the `forecast` package so we load this first, then call the data.    
+### (Original source: http://data.is/TSDLdemo)
 
-(Original source: http://data.is/TSDLdemo)
-
-```{r load forecast and data, message=FALSE}
+install.packages('forecast', dependencies = TRUE)
 library(forecast)
 data("wineind")
-```
+
 
 #### (iii) Explore Data
-```{r explore wineind, eval = FALSE}
 ?wineind
 class(wineind)
-```
-
-```{r view wineind, eval = FALSE}
 wineind
-```
 
-```{r plot wineind}
 plot(wineind) #base R plot
-```
+
 
 #### (iv) Apply Decomposition algorithm
+###Apply `stl` algorithm, which divides the time series into 3 components, trend, seasonality & remainder, using Loess (a method for estimating nonlinear relationships).
+### `stl` documentation: https://stat.ethz.ch/R-manual/R-devel/library/stats/html/stl.html 
 
-Apply `stl` algorithm, which divides the time series into 3 components, trend, seasonality & remainder, using Loess (a method for estimating nonlinear relationships).
-
-`stl` documentation: https://stat.ethz.ch/R-manual/R-devel/library/stats/html/stl.html 
-
-```{r stl_wine}
 stl_wine <- stl(wineind, s.window = "periodic") #s.window controls variation of seasonal component
-```
-
-```{r stl_wine data, eval=FALSE}
 stl_wine
-```
-
-```{r stl_wine plot}
 plot(stl_wine) #base R plot
-```
+
 
 #### (v) Prepare decomposed data as a single xts object
-First, convert each decomposed component into a separate `ts` (time series) object, and then into `xts` (extensible time series) object:
-  
-  ```{r convert to xts}
-#(1980, 1) is start year & unit of observations, 12 is the no. of observations per period i.e. monthly
-
-random_stl <- ts(stl_wine$time.series[,3], start = c(1980, 1), frequency=12)
-random_stl.xts <- as.xts(random_stl)
+#### 1. First, convert each decomposed component into a separate `ts` (time series) object, 
+#### 2. Then convert into `xts` (extensible time series) object:
+#### (1980, 1) is start year & unit of observations, 12 is the no. of observations per period i.e. monthly
 
 seasonal_stl <- ts(stl_wine$time.series[,1], start = c(1980, 1), frequency=12)
 seasonal_stl.xts <- as.xts(seasonal_stl)
 
 trend_stl <- ts(stl_wine$time.series[,2], start = c(1980, 1), frequency=12)
 trend_stl.xts <- as.xts(trend_stl)
-```
 
-Secondly, column bind the 3 `xts` components together to create a single `xts` object for plotting:
-  
-  ```{r 3 to 1}
+random_stl <- ts(stl_wine$time.series[,3], start = c(1980, 1), frequency=12)
+random_stl.xts <- as.xts(random_stl)
+
+#### Secondly, column bind the 3 `xts` components together to create a single `xts` object for plotting (3 to 1):
+
 wine.plot <- cbind(random_stl.xts, seasonal_stl.xts, trend_stl.xts)
 
 #re-name columns
 colnames(wine.plot) <- c("remainder", "seasonal", "trend")
-```
+
 
 #### (vi) Create Dygraph
-Basic:
-  ```{r uncustomised dygraph, eval=FALSE}
+
 dygraph(wine.plot) %>%
   dyOptions(stackedGraph = TRUE)
-```
+
 
 #### (vii) Customise Dygraph
-Add Title & Range Selector & change colours:
-  ```{r range, eval=FALSE}
+#### Add Title & Range Selector & change colours:
+
 dygraph(wine.plot, main = "Australian Wine Sales") %>%
   dyOptions(stackedGraph = TRUE, colors=RColorBrewer::brewer.pal(3, "Set1"))%>%
   dyRangeSelector()
-```
+______________________________________________________________________________________
 
----
-  
-  ### B. Heatmap
-  Produce a D3 heatmap using the `d3heatmap` R library.
+### B. Heatmap
+#### Produce a D3 heatmap using the `d3heatmap` R library.
 
 #### (i) Call d3heatmap library
 
-```{r install and call heatmaps}
+devtools::install_github("rstudio/d3heatmap")
 library(d3heatmap)
-```
 
 #### (ii) Load Data
-Data created from personal burger blog, in `wide` format necessary to use this library. (https://burgerite.blogspot.co.uk)    
+####Data created from a personal burger blog, in `wide` format necessary to use this library. (https://burgerite.blogspot.co.uk)    
+#### Data available here: https://github.com/RLadiesCodingLondon/MayEvent-Chiin-htmlwidgets
 
-Data available here: https://github.com/RLadiesCodingLondon/MayEvent-Chiin-htmlwidgets
-
-```{r load burger data, eval=FALSE}
+setwd("~/STEM_Education/Data Analysis Lessons/4. JAVA WITH R") # the path to your csv file
+getwd()
 burger <- read.csv("burger.csv", header = TRUE, row.names = 1)
-```
 
 #### (iii) Explore Data
-5 attributes, 25 burgers reviewed.
-```{r explore burger, eval=FALSE}
+### 5 attributes, 25 burgers reviewed.
+
 View(burger)
-```
+
 
 #### (iv) Create Heatmap
-Blue = higher score, Red = lower score
-```{r uncustomised heatmap, eval=FALSE}
-d3heatmap(burger)
-```
+### Blue = higher score, Red = lower score
 
----
+d3heatmap(burger)
+______________________________________________________________________________________
   
-  ### C. 3D Scatterplot
-  Produce a 3D scatterplot using the `threejs` R library using same `burger` data.
+### C. 3D Scatterplot
+### Produce a 3D scatterplot using the `threejs` R library using same `burger` data.
 
 #### (i) Call threejs library
 
-```{r install and call scatterplot}
 library(threejs)
-```
 
 #### (ii) Create Scatterplot
 
-```{r thing scatterplot, eval=FALSE}
 scatterplot3js(burger$proportions, burger$ingredients, burger$patty, flip.y=FALSE) #flip.y to control direction of y-axis
-```
 
 #### (iii) Customise Scatterplot
 
-```{r other thing scatterplot, eval=FALSE}
 label <- c("proportions", "patty", "ingredients") #define axis labels
 
 scatterplot3js(burger$proportions, burger$ingredients, burger$patty, 
@@ -186,39 +139,39 @@ scatterplot3js(burger$proportions, burger$ingredients, burger$patty,
                stroke = NULL,
                size=burger$value/5,
                renderer = "canvas")
-```
-
----
+______________________________________________________________________________________
   
-  ### D. 3D Globe
-  Produce a 3D globe, again using the `threejs` R library.
+### D. 3D Globe
+### Produce a 3D globe, again using the `threejs` R library.
 
 #### (i) Load Data
-Geo-location data from Google Analytics of burger blog traffic.
+### Geo-location data from Google Analytics of burger blog traffic.
+### Data available here: https://github.com/RLadiesCodingLondon/MayEvent-Chiin-htmlwidgets
 
-Data available here: https://github.com/RLadiesCodingLondon/MayEvent-Chiin-htmlwidgets
-```{r load globe data}
+setwd("~/STEM_Education/Data Analysis Lessons/4. JAVA WITH R") # the path to your csv file
+getwd()
+
 views <- read.csv("globejs.csv", header = TRUE, row.names = 1)
-```
+
 
 #### (ii) Explore Data
-169 cities with cumulative blog page views >= 2 since Sep '15.
-```{r explore traffic, eval=FALSE}
+### 169 cities with cumulative blog page views >= 2 since Sep '15.
+
 View(views)
-```
+
 
 #### (iii) Create Globe
 
-```{r unustomised globe, eval=FALSE}
+
 globejs(lat = views$Lat, long = views$Long, value = views$PageViews)
-```
+
 
 #### (iv) Customise Globe
 
-```{r customised globe, eval=FALSE}
+
 traffic <- 10000*views$PageViews/max(views$PageViews)
 
 globejs(lat = views$Lat, long = views$Long, value = traffic,
 atmosphere = TRUE,
 pointsize = 0.5)
-```
+
