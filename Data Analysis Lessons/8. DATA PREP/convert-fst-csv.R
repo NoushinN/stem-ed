1739f8ea6d36ffeb08cbcd5506a9328c8447acce
Noushin Nabavi <nabavinoushin@gmail.com>
2019-04-09 23:44:15 -0700

    data prep function

---
+++ Data Analysis Lessons/8. DATA PREP/convert-fst-csv.R
@@ -0,0 +1,33 @@
+###DEMO for converting fst to csf files in R ###
+# lessons curated by Noushin Nabavi, PhD (adapted from Datacamp lessons)
+
+# load dependencies
+library(fst)
+library(fs)
+library(here)
+
+# use here
+set_here("xyz")
+
+# read fst and write csv
+x_fst <- read_fst("xx.fst")
+fwrite(x_fst, here("xx.csv"))
+
+# ---------------------------------------------------------
+
+path <- ("xyz")
+paths <- list.files("xyz", pattern = "*.fst")
+print(paths)
+
+fst_to_csv <- function(path) {
+  
+  # set file names
+  path %>%
+    basename() %>%
+    tools::file_path_sans_ext() %>%
+    map(function(p) list("name" = p, "content" = read_fst(past0(file_path, p, ".fst")))) %>%
+    map (function(q) write.csv(q$content, file = paste(q$name, ".csv")))
+    
+}
+
+x <- fst_to_csv(paths)
