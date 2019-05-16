64fbece00807367532dbdc429045d4955e141191
Noushin Nabavi <nabavinoushin@gmail.com>
2019-05-15 20:45:28 -0700

    speed read

--- Data Analysis Lessons/8. DATA PREP/data-import-speed.R
+++ Data Analysis Lessons/8. DATA PREP/data-import-speed.R
@@ -1,10 +1,5 @@
-## This test script works on my MacBook running
-## R3.6.0 & RStudio 1.2.1335 with all CRAN packages
-## up-to-date
 
-## Some of the test code does NOT work in the SAE,
-## or it works with some software and not others
-
+# compare multiple packages speed in reading a zipped file
 library(vroom)
 library(feather)
 library(here)
@@ -16,11 +11,13 @@ df <- iris
 # works with RStudio + R3.5.3 32-bit 
 # works with R3.5.3 32-bit GUI
 # but bonks through R3.5.3 62-bit GUI & Microsoft R Open 3.5.1 GUI
+
 vroom_write(df, "test.csv.gz", delim = ",")
 
 # bonks with RStudio + default setting of Microsoft R Open 3.5.1
 # bonks with with RStudio + R3.5.3 62-bit (and the R3.5.3 62-bit GUI)
 # works with RStudio + R3.5.3 32-bit (and the R3.5.3 32-bit GUI)
+
 iris_vroom<- vroom("test.csv.gz", delim = ",")
 
 # works with all options
@@ -31,6 +28,7 @@ iris_feather <- read_feather("test.feather")
 
 # but this hack works with all options (the default behaviour is
 # is meant to be read all columns)
+
 colnms <- colnames(df)
 iris_feather <- read_feather("test.feather", columns = colnms)
 
