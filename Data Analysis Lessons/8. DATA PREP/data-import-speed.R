694ed30eaf448c1b02de50f59a91ca99344e0349
Noushin Nabavi <nabavinoushin@gmail.com>
2019-05-15 21:12:04 -0700

    speed

--- Data Analysis Lessons/8. DATA PREP/data-import-speed.R
+++ Data Analysis Lessons/8. DATA PREP/data-import-speed.R
@@ -6,29 +6,18 @@ library(here)
 
 df <- iris
 
-# works with RStudio + default setting of Microsoft R Open 3.5.1
-# works with RStudio + R3.5.3 62-bit 
-# works with RStudio + R3.5.3 32-bit 
-# works with R3.5.3 32-bit GUI
-# but bonks through R3.5.3 62-bit GUI & Microsoft R Open 3.5.1 GUI
-
+# write with vroom
 vroom_write(df, "test.csv.gz", delim = ",")
 
-# bonks with RStudio + default setting of Microsoft R Open 3.5.1
-# bonks with with RStudio + R3.5.3 62-bit (and the R3.5.3 62-bit GUI)
-# works with RStudio + R3.5.3 32-bit (and the R3.5.3 32-bit GUI)
-
+# read with vroom
 iris_vroom<- vroom("test.csv.gz", delim = ",")
 
-# works with all options
+# write with feather
 write_feather(df, "test.feather")
 
-# bonks with all options
+# read with feather
 iris_feather <- read_feather("test.feather")
 
-# but this hack works with all options (the default behaviour is
-# is meant to be read all columns)
 
 colnms <- colnames(df)
 iris_feather <- read_feather("test.feather", columns = colnms)
-
