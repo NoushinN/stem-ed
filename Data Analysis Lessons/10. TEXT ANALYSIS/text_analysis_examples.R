f80527ec68022c32fc6f82f1579643f0fb47046c
Noushin Nabavi <nabavinoushin@gmail.com>
2019-06-04 20:17:51 -0700

    code update

--- Data Analysis Lessons/10. TEXT ANALYSIS/text_analysis_examples.R
+++ Data Analysis Lessons/10. TEXT ANALYSIS/text_analysis_examples.R
@@ -14,8 +14,8 @@ library(tidytext)
 url = "https://d1p17r2m4rzlbo.cloudfront.net/wp-content/uploads/2016/03/Airline-Sentiment-2-w-AA.csv"
 sentiment_data <- read_csv(url)
 
-
-negative_sents <- sentiment_data %>%
+# compare group_by() with ungroup()
+negative_sents1 <- sentiment_data %>%
   # Filter to only choose the words associated with sadness
   filter(airline_sentiment == "negative") %>%
   # Group by word
@@ -25,9 +25,35 @@ negative_sents <- sentiment_data %>%
   # Arrange to sort in order of descending frequency
   arrange(desc(airline_sentiment_conf))
 
-negative_sents %>%
+#ungroup()
+negative_sents2 <- sentiment_data %>%
+  # Filter to only choose the words associated with sadness
+  filter(airline_sentiment == "negative") %>%
+  # Group by word
+  group_by(negativereason) %>%
+  # Use the summarize verb to find the mean frequency
+  summarize(airline_sentiment_conf = mean(`airline_sentiment:confidence`)) %>%
+  # Arrange to sort in order of descending frequency
+  arrange(desc(airline_sentiment_conf)) %>%
+  ungroup()
+
+# compare plots1 and 2 with one another
+require(gridExtra)
+plot1 <- negative_sents1 %>%
   top_n(20) %>%
   mutate(word = reorder(negativereason, airline_sentiment_conf)) %>%
   ggplot(aes(negativereason, airline_sentiment_conf)) +
   geom_col() +
   coord_flip()
+
+
+plot2 <-negative_sents2 %>%
+  top_n(20) %>%
+  mutate(word = reorder(negativereason, airline_sentiment_conf)) %>%
+  ggplot(aes(negativereason, airline_sentiment_conf)) +
+  geom_point() +
+  coord_flip()
+grid.arrange(plot1, plot2, ncol=2)
+
+#-------------------------------------------------------------------------------
+
