2b81eb046330f631bb171a28ec190c523c36f4b8
Noushin Nabavi <nabavinoushin@gmail.com>
2019-06-20 18:46:28 -0700

    linear regression

--- Data Analysis Lessons/3. ML/logistic-regression.R
+++ Data Analysis Lessons/3. ML/logistic-regression.R
@@ -6,4 +6,108 @@ library(broom)
 library(sigr)
 library(tidyverse)
 
+
+# load data for logistic regression analysis
+library(survival)
+library(survminer)
+data(lung)
+dim(lung)
+
+lung$survival <- rep(c("survived", "perished"), 114)
+
+lung <- na.omit(lung)
+
 # Logistic regression to predict probabilities
+summary(lung)
+
+# Create the survived column
+lung$survived <- lung$survival == "survived"
+
+# Create the formula
+(fmla <- survived ~ pat.karno + ph.karno  + ph.ecog)
+
+# Fit the logistic regression model
+lung_model <- glm(fmla, data = lung, family = binomial)
+
+# Call summary
+summary(lung_model)
+
+# Call glance
+(perf <- glance(lung_model))
+
+# Calculate pseudo-R-squared
+(pseudoR2 <- 1 - perf$deviance/perf$null.deviance)
+
+# to evaluate performance of a model for logistic regression, we use pseudoR2, glm, and family = binomial
+
+# ------------------------------------------------------------------------------
+
+# make predictions on the model that was developed previously 
+
+# Make predictions
+lung$pred <- predict(lung_model, type = "response")
+
+# Look at gain curve
+GainCurvePlot(lung, "pred", "survival", "lung survival model")
+
+# ------------------------------------------------------------------------------
+
+# Poisson and quasipoisson regression to predict counts
+# When the variance is much larger than the mean, the Poisson assumption doesn't apply, and one solution is to use quasipoisson regression, which does not assume that variance=mean
+
+data("diabetic")
+str(diabetic)
+
+# Create the formula string for status rented as a function of other inputs
+(fmla <- (status ~ time + risk + trt + eye + age))
+
+# Calculate the mean and variance of the outcome
+(mean_status <- mean(diabetic$status))
+(var_status <- var(diabetic$status))
+
+# Fit the model
+diabetic_model <- glm(fmla, data = diabetic, family = quasipoisson)
+
+# Call glance
+(perf <- glance(diabetic_model))
+
+# Calculate pseudo-R-squared
+(pseudoR2 <- 1 - perf$deviance/perf$null.deviance)
+
+# Make predictions on diabetic data
+diabetic$pred <- predict(diabetic_model, newdata = diabetic, type = "response")
+
+# Calculate the RMSE
+diabetic %>% 
+  mutate(residual = pred - time) %>%
+  summarize(rmse  = sqrt(mean(residual^2)))
+
+# Plot predictions vs time (pred on x-axis)
+ggplot(diabetic, aes(x = pred, y = time)) +
+  geom_point() + 
+  geom_abline(color = "darkblue")
+
+
+# visualize predictions
+# Plot predictions and cnt by date/time
+diabetic %>% 
+  # set start time to 0, convert unit to days
+  mutate(instant = (time - min(time))/24) %>%  
+  # gather cnt and pred into a value column
+  gather(key = valuetype, value = value, time, pred) %>%
+  #filter(time > 0) %>% # restrict to first 14 days
+  # plot value by instant
+  ggplot(aes(x = instant, y = value, color = valuetype, linetype = valuetype)) + 
+  geom_point() + 
+  geom_line() + 
+  scale_x_continuous("Day", breaks = 0:14, labels = 0:14) + 
+  scale_color_brewer(palette = "Dark2") + 
+  ggtitle("Predicted diabetes survival time, Quasipoisson model")
+
+# ------------------------------------------------------------------------------
+
+# generalized additive models (GAM) to learn non-linear transforms
+
+
+
+
---
+++ README.md
@@ -0,0 +1,19 @@
+## Teaching, Learning, and Research
+
+Hello-out-there,
+
+We live in a complex world and we are not prepared for it. In my learning journey, I have been fortunate to benefit from some highly advanced teachers and mentors who taught me to not only learn the lesson materials but learn to know, to do, to live, to be, and to transform myself and the society. To pass this knowledge to the next generation of learners, I myself went on a journey to learn teaching. I have participated in a number of teaching and learning classes in the recent past and have been inspired by several core ideas and philosophies about teaching, learning, and STEM education that I like to implement in my own teaching practices.
+
+It is my passion to enhance excellence in education by implementing modern and effective teaching practices for diverse learners. For example, I myself am a visual learner and do recognize when my mind wanders when the lesson materials miss visuals. Recognizing that learning styles are quite diverse for participants, the onus is on instructors to develop materials that cater to every learning type be it visual, auditory, abstract/mathematical, spatial, or lingual learners. My goal as an instructor is to be a constructive agent for a learning journey towards success. 
+
+Improvement of teaching and learning is a dynamic and ongoing process, just as is research in any STEM discipline. At the core of improving teaching and learning is the need to accurately determine what we have learned. In applying a research approach to teaching, I strive to apply these **7 processes**: 
+  
+**(i)** learning foundational knowledge,   
+**(ii)** creating objectives for learning,  
+**(iii)** developing an hypothesis for practices to achieve the learning objectives,  
+**(iv)** defining measures of success,  
+**(v)** developing and implementing teaching practices within an experimental design,  
+**(vi)** collecting and analyzing data from lessons,  
+**(vii)** reflecting, evaluating, and iterating lessons.  
+
+Some of the lessons I have curated (both independently and modified from other sources) for data science can be found [here](https://github.com/NoushinN/STEM_Education/tree/master/Data%20Analysis%20Lessons).  This is all possible thanks to both teaching and data science community for sharing their knowledge openly. Some of the literature that have inspired this philosophy can be found [here](https://github.com/NoushinN/STEM_Teaching_Learning_Research/tree/master/Teaching%20Literature).
